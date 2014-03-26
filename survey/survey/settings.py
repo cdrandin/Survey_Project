5342b42448d191f2b6fe5cfd50bbeade516d93fc
cdrandin <cdrandin@hotmail.com>
2014-03-25 17:35:11 -0700

    Added django-google-charts to settings.py and installed in vitualenv env

--- survey/survey/settings.py
+++ survey/survey/settings.py
@@ -128,6 +128,8 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'survey_page',
+    'googlecharts',
+    
     # Uncomment the next line to enable the admin:
     #'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
