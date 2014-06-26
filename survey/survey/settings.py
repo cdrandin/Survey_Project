70ccf1dbb32bddf3c8bafde51a9620b6bee3f161
Christopher Randin <cdrandin@hotmail.com>
2014-06-26 14:01:07 -0700

    Clean misc files. Added correct static files

--- survey/survey/settings.py
+++ survey/survey/settings.py
@@ -70,7 +70,8 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = os.path.join(PROJECT_DIR, '../survey_page/static ')#static_resources') #''
+#STATIC_ROOT = os.path.join(PROJECT_DIR, '../survey_page/static ')#static_resources') #''
+STATIC_ROOT = os.path.join(os.path.dirname(__file__), 'static/')
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
